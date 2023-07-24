cbc73275541160e7dd30e257d997e92d4455990e
Louis <louis.dupraz@epitech.eu>
2023-07-24 20:39:52 +0200

    fix: missing '}'

--- run.cmd
+++ run.cmd
@@ -8,7 +8,7 @@ whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 SET mypath=%~dp0
 cd %mypath%
 if (Test-Path "CheckInstall.exe") {
-    powershell.exe -Command "& CheckInstall.exe %*}"
+    powershell.exe -Command "& {CheckInstall.exe %*}"
 } else {
     powershell.exe -Command "& {python src/main.py %*}"
 }
