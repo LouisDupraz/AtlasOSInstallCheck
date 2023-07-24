9c185162be6ac7e5e6938733c54a8ecfaf4a6c8c
Louis <louis.dupraz@epitech.eu>
2023-07-24 21:01:22 +0200

    fix: using sys.exit insteat of builtin exit

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from sys import argv
+from sys import argv, exit
 from os import listdir
 from registry import checkKeyExistsAndDelete, checkValueExistsAndDelete, checkAndResetValue
 from files import checkFileExistsAndDelete
